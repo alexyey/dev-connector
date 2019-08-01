4974af57087e4c8c5915b0bd4e10f911b91cda36
Alessio Cialini <acialini@key-biz.it>
2019-08-01 17:39:26 +0200

    introduced token creation into user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
-
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/Users');
@@ -58,8 +59,22 @@ router.post(
 
             await user.save();
 
-            // Return jsonwebtoken
-            res.send('User registered');
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                {expiresIn: 3600},
+                (err, token) => {
+                    if(err) throw err;
+                    res.json({ token });
+                }
+            );
+
         } catch (e) {
             console.error(e.message);
             return res.status(500).send('Server error');

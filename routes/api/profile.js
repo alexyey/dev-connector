09d0391da3990b5f8e69cd6b6cd8839ed5f654e6
Alessio Cialini <acialini@key-biz.it>
2019-08-03 16:27:36 +0200

    introduced experience removal for profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -224,4 +224,29 @@ router.put(
     }
 );
 
+// @route  DELETE api/profile/experience/:exp_id
+// @desc   Delete profile experience
+// @access Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        if (!profile) return res.status(400).json({ msg: 'Profile not found'});
+
+        //Get remove index
+        const removeIndex = profile.experience
+            .map(item => item.id)
+            .indexOf(req.params.exp_id);
+
+        profile.experience.splice(removeIndex, 1);
+
+        await profile.save();
+
+        return res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;

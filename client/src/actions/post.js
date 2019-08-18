746b32ffe82dcbf6989ba4e7c50d59d5d5e4265a
Alessio Cialini <acialini@key-biz.it>
2019-08-18 17:57:18 +0200

    Introducing Posts like/dislike functionalities

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { setAlert } from "./alert";
 import {
     GET_POSTS,
-    POST_ERROR
+    POST_ERROR, UPDATE_LIKES
 } from "./types";
 
 // Get posts
@@ -21,4 +21,39 @@ export const getPosts = () => async dispatch => {
         });
     }
 };
+
+// Add like
+export const addLike = postId => async dispatch => {
+    try {
+        const res = await axios.put(`/api/posts/like/${postId}`);
+
+        dispatch({
+            type: UPDATE_LIKES,
+            payload: { id: postId, likes: res.data }
+        });
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
+// Remove like
+export const removeLike = postId => async dispatch => {
+    try {
+        const res = await axios.put(`/api/posts/unlike/${postId}`);
+
+        dispatch({
+            type: UPDATE_LIKES,
+            payload: { id: postId, likes: res.data }
+        });
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
 
